9082be404de933899f6f2eca2532b5952a248b73
greduan <me@greduan.com>
2023-04-16 10:57:08 +0200

    feat: handle conversations instead of single prompts

--- vehicle_genius_portal/settings.py
+++ vehicle_genius_portal/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', '0') == '1'
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
     'localhost',
     'test.vehiclegenius.site',
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -13,11 +13,11 @@ def index(request):
 
 
 def index_post(request):
-    data = request.POST
-    print(data['vin'], data['prompt'])
+    data = parse_request_post(request.POST)
+    print(data['vin'], data['messages'])
     body = {
         'vin': data['vin'],
-        'prompt': data['prompt'],
+        'messages': data['messages'],
     }
     headers = {
         'Content-Type': 'application/json',
@@ -27,13 +27,40 @@ def index_post(request):
         os.environ.get('API_BASE_URL') + '/assistant/answer-user-prompt',
         data=json.dumps(body),
         headers=headers)
-    answer = response.content.decode('utf-8')
+    answer = json.loads(response.content.decode('utf-8'))
     return render(request, 'vehicles/index.html', {
-        'show_answer': True,
-        'question': data['prompt'],
-        'answer': answer,
+        'question': data['messages'][-1]['content'],
+        'ai_messages': answer,
     })
 
 
 def index_get(request):
     return render(request, 'vehicles/index.html')
+
+
+def parse_request_post(request_post):
+    # create a new copy of the request.POST data
+    parsed_post = request_post.copy()
+
+    # loop through the keys in the request.POST data
+    for key in request_post:
+        # check if the key contains an array index
+        if '[' in key and ']' in key:
+            # extract the key prefix and index from the key name
+            key_prefix, index = key.split('[', 1)
+            index = index.split(']', 1)[0]
+            # create a dictionary for the message
+            message = {k.split('.')[1]: v for k, v in request_post.items() if k.startswith(f'{key_prefix}[{index}].')}
+            # add the message to the parsed_post dictionary
+            parsed_post[key_prefix] = parsed_post.get(key_prefix, []) + [message]
+
+    # remove duplicates from lists in the parsed_post dictionary
+    for key, value in parsed_post.items():
+        if isinstance(value, list):
+            unique_value = []
+            for item in value:
+                if item not in unique_value:
+                    unique_value.append(item)
+            parsed_post[key] = unique_value
+
+    return parsed_post
